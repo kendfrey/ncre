528919fd66066ef42d4550fbf1b4bb1034a41378
Kendall Frey <kendfrey@gmail.com>
2017-12-28 18:52:35 -0500

    Reorganize tests

--- test/test.js
+++ test/test.js
@@ -12,66 +12,24 @@ const dotnet =
 dotnet.parse({ regex: "" }, true);
 
 const assert = require("assert");
-describe("Regex", () =>
+suite("regex engine", () =>
 {
-	describe("constructor()", () =>
+	suite("basics", () =>
 	{
-		testParseError("invalid optional - ?", "?a");
-		testParseError("invalid repetition - *", "*a");
-		testParseError("invalid repetition - +", "+a");
-		testParseError("mismatched parentheses - (", "(a");
-		testParseError("mismatched parentheses - )", ")a");
-		testParseError("invalid group name", "(?<1a>)");
-		testParse("underscored group name", "(?<_1a>)");
-		testParseError("zero group number", "(?<0>)");
-		testParseError("invalid regex flag", "(?a)");
-		testParseError("missing regex flags", "(?)");
-		testParseError("mismatched brackets - [", "[");
-		testParse("mismatched brackets - ]", "]");
-		testParseError("invalid escape", "\\q");
-		testParseError("invalid class escape", "[\\q]");
-	});
-	describe("match()", () =>
-	{
-		testMatch("failed match", "a", "b");
 		testMatch("literal text", "abb", "ababbb");
-		testMatch("optional - ?", "ab?c?a?", "aba");
-		testMatch("repetition - *", "b*a*", "aabbb");
-		testMatch("repetition - +", "b+", "aabbb");
-		testMatch("lazy optional - ??", "ab??", "ab");
-		testMatch("lazy repetition - *?", "a*?b*?", "aabbb");
-		testMatch("lazy repetition - +?", "a+?b+?", "aabbb");
-		testMatch("non-capturing groups - (?:)", "a(?:b(?:c)?)*", "abbccb");
-		testMatch("escaping - \\", "a\\+", "aaa+");
-		testMatch("capturing groups - ()", "(a(b)*)*(b)", "aabbbaab");
-		testMatch("named capturing groups - (?<>)", "(?<A>a)+(b)", "aab");
-		testMatch("named capturing groups - (?'')", "(?'A'a)+(b)", "aab");
-		testMatch("duplicate named capturing groups", "(?<A>a)+(?<A>b)", "aab");
-		testMatch("case sensitive named capturing groups", "(?<A>a)+(?<a>b)", "aab");
-		testMatch("numbered capturing groups", "(?<3>a)+(b)", "aab");
-		testMatch("duplicate numbered capturing groups", "(a)+(?<1>b)", "aab");
-		testMatch("inline flags - (?)", "a(?i)a*(?i-i)a", "AaAaA");
-		testMatch("scoped inline flags - (?)", "(?:a(?i)a*)a", "AaAaA");
-		testMatch("scoped flags - (?:)", "a(?i:a*)a", "AaAaA");
-		testMatch("case sensitivity", "a", "Aa");
-		testMatch("case insensitivity", "a", "Aa", { flags: "i" });
-		testMatch("character classes - []", "[a-z0]+", "123abc0123");
-		testMatch("negated character classes - [^]", "[^a-z0]+", "123abc0123");
-		testMatch("character class escaping - \\", "[[\\]a]+", "ab][[]ab");
-		testMatch("character class literal hyphens", "[a-]+", "a-z");
-		testMatch("character class subtraction - -[]", "[a-z-[ad]]+", "abcdef");
-		testMatch("digit class - \\d", "\\d+", "abc1234567890def");
-		testMatch("non-digit class - \\D", "\\D+", "abc1234567890def");
-		testMatch("word class - \\w", "\\w+", "(a_123)");
-		testMatch("non-word class - \\W", "\\W+", "(a_123)");
-		testMatch("whitespace class - \\s", "\\s+", "a\r \nb");
-		testMatch("non-whitespace class - \\S", "\\S+", "a\r \nb");
-		testMatch("digit subclass - [\\d]", "[\\d]+", "abc1234567890def");
-		testMatch("non-digit subclass - [\\D]", "[\\D]+", "abc1234567890def");
-		testMatch("word subclass - [\\w]", "[\\w]+", "(a_123)");
-		testMatch("non-word subclass - [\\W]", "[\\W]+", "(a_123)");
-		testMatch("whitespace subclass - [\\s]", "[\\s]+", "a\r \nb");
-		testMatch("non-whitespace subclass - [\\S]", "[\\S]+", "a\r \nb");
+		testMatch("failed match", "a", "b");
+	});
+
+	suite("escape sequences", () =>
+	{
+		testMatch("literal - \\", "a\\+", "aaa+");
+		testParseError("invalid sequence", "\\q");
+		testMatch("digit - \\d", "\\d+", "abc1234567890def");
+		testMatch("non-digit - \\D", "\\D+", "abc1234567890def");
+		testMatch("word character - \\w", "\\w+", "(a_123)");
+		testMatch("non-word character - \\W", "\\W+", "(a_123)");
+		testMatch("whitespace - \\s", "\\s+", "a\r \nb");
+		testMatch("non-whitespace - \\S", "\\S+", "a\r \nb");
 		testMatch("tab - \\t", "\\t+", "\ta");
 		testMatch("carriage return - \\r", "\\r+", "\ra");
 		testMatch("newline - \\n", "\\n+", "\na");
@@ -79,61 +37,165 @@ describe("Regex", () =>
 		testMatch("escape - \\e", "\\e+", "\x1Ba");
 		testMatch("form feed - \\f", "\\f+", "\fa");
 		testMatch("vertical tab - \\v", "\\v+", "\va");
-		testMatch("tab class - [\\t]", "[\\t]+", "\ta");
-		testMatch("carriage return class - [\\r]", "[\\r]+", "\ra");
-		testMatch("newline class - [\\n]", "[\\n]+", "\na");
-		testMatch("alert class - [\\a]", "[\\a]+", "\x07a");
-		testMatch("backspace class - [\\b]", "[\\b]+", "\ba");
-		testMatch("escape class - [\\e]", "[\\e]+", "\x1Ba");
-		testMatch("form feed class - [\\f]", "[\\f]+", "\fa");
-		testMatch("vertical tab class - [\\v]", "[\\v]+", "\va");
+	});
+
+	suite("repetition", () =>
+	{
+		testMatch("zero or one - ?", "ab?c?a?", "aba");
+		testMatch("zero or more - *", "b*a*", "aabbb");
+		testMatch("one or more - +", "b+", "aabbb");
+		testParseError("invalid character - ?", "?a");
+		testParseError("invalid character - *", "*a");
+		testParseError("invalid character - +", "+a");
+
+		suite("lazy", () =>
+		{
+			testMatch("lazy zero or one - ??", "ab??", "ab");
+			testMatch("lazy zero or more - *?", "a*?b*?", "aabbb");
+			testMatch("lazy one or more - +?", "a+?b+?", "aabbb");
+		});
+	});
+
+	suite("groups", () =>
+	{
+		testMatch("non-capturing - (?:)", "a(?:b(?:c)?)*", "abbccb");
+		testParseError("mismatched parentheses - (", "(a");
+		testParseError("mismatched parentheses - )", ")a");
+
+		suite("capturing", () =>
+		{
+			testMatch("captured - ()", "(a(b)*)*(b)", "aabbbaab");
+			testMatch("named - (?<>)", "(?<A>a)+(b)", "aab");
+			testMatch("named - (?'')", "(?'A'a)+(b)", "aab");
+			testMatch("numbered", "(?<3>a)+(b)", "aab");
+			testMatch("duplicate names", "(?<A>a)+(?<A>b)", "aab");
+			testMatch("duplicate numbers", "(a)+(?<1>b)", "aab");
+			testMatch("case sensitive names", "(?<A>a)+(?<a>b)", "aab");
+			testParseError("invalid names", "(?<1a>)");
+			testParse("underscored names", "(?<_1a>)");
+			testParseError("invalid zero", "(?<0>)");
+		});
+	});
+
+	suite("character classes", () =>
+	{
+		testMatch("inclusive - []", "[a-z0]+", "123abc0123");
+		testMatch("exclusive - [^]", "[^a-z0]+", "123abc0123");
+		testParseError("mismatched brackets - [", "[");
+		testParse("mismatched brackets - ]", "]");
+		testMatch("literal hyphens - -", "[a-]+", "a-z");
+		testMatch("subtraction - -[]", "[a-z-[ad]]+", "abcdef");
+
+		suite("escape sequences", () =>
+		{
+			testMatch("literal - \\", "[[\\]a]+", "ab][[]ab");
+			testParseError("invalid sequence", "[\\q]");
+			testMatch("digit - \\d", "[\\d]+", "abc1234567890def");
+			testMatch("non-digit - \\D", "[\\D]+", "abc1234567890def");
+			testMatch("word character - \\w", "[\\w]+", "(a_123)");
+			testMatch("non-word character - \\W", "[\\W]+", "(a_123)");
+			testMatch("whitespace - \\s", "[\\s]+", "a\r \nb");
+			testMatch("non-whitespace - \\S", "[\\S]+", "a\r \nb");
+			testMatch("tab - \\t", "[\\t]+", "\ta");
+			testMatch("carriage return - \\r", "[\\r]+", "\ra");
+			testMatch("newline - \\n", "[\\n]+", "\na");
+			testMatch("alert - \\a", "[\\a]+", "\x07a");
+			testMatch("backspace - \\b", "[\\b]+", "\ba");
+			testMatch("escape - \\e", "[\\e]+", "\x1Ba");
+			testMatch("form feed - \\f", "[\\f]+", "\fa");
+			testMatch("vertical tab - \\v", "[\\v]+", "\va");
+		});
+	});
+
+	suite("flags", () =>
+	{
+		testMatch("case sensitivity", "a", "Aa");
+		testMatch("case insensitivity", "a", "Aa", { flags: "i" });
+
+		suite("inline", () =>
+		{
+			testMatch("inline - (?)", "a(?i)a*(?i-i)a", "AaAaA");
+			testMatch("scoped inline - (?)", "(?:a(?i)a*)a", "AaAaA");
+			testMatch("scoped - (?:)", "a(?i:a*)a", "AaAaA");
+			testParseError("invalid flag", "(?a)");
+			testParseError("missing flags", "(?)");
+		});
 	});
 });
 
-describe("Match", () =>
+suite("API", () =>
 {
-	describe("group()", () =>
+	suite("Regex", () =>
 	{
-		it("numbered groups", () =>
+		suite("constructor()", () =>
 		{
-			const match = new ncre.Regex("(a+)b+(b+)?").match("aaabbb");
-			assert.ok(match);
-			assert.strictEqual(match.success, true);
-			assert.ok(match.group(1));
-			assert.ok(match.group(2));
-			assert.strictEqual(match.group(1).success, true);
-			assert.strictEqual(match.group(2).success, false);
+			test("parse", () =>
+			{
+				assert.doesNotThrow(() => new ncre.Regex("a"));
+			});
 		});
-		it("named groups", () =>
+		suite("match()", () =>
 		{
-			const match = new ncre.Regex("(a+)b+(?<A>b+)?").match("aaabbb");
-			assert.ok(match);
-			assert.strictEqual(match.success, true);
-			assert.ok(match.group("1"));
-			assert.ok(match.group("A"));
-			assert.strictEqual(match.group("1").success, true);
-			assert.strictEqual(match.group("A").success, false);
+			test("success", () =>
+			{
+				const match = new ncre.Regex("a").match("a");
+				assert.ok(match);
+				assert.strictEqual(match.success, true);
+			});
+			test("failure", () =>
+			{
+				const match = new ncre.Regex("a").match("b");
+				assert.ok(match);
+				assert.strictEqual(match.success, false);
+			});
 		});
-		it("nonexistent numbered groups", () =>
+	});
+
+	suite("Match", () =>
+	{
+		suite("group()", () =>
 		{
-			const match = new ncre.Regex("a(b)").match("ab");
-			assert.ok(match);
-			assert.strictEqual(match.success, true);
-			assert.strictEqual(match.group(2), undefined);
-		});
-		it("nonexistent named groups", () =>
-		{
-			const match = new ncre.Regex("a(?<A>b)").match("ab");
-			assert.ok(match);
-			assert.strictEqual(match.success, true);
-			assert.strictEqual(match.group("B"), undefined);
+			test("numbered groups", () =>
+			{
+				const match = new ncre.Regex("(a+)b+(b+)?").match("aaabbb");
+				assert.ok(match);
+				assert.strictEqual(match.success, true);
+				assert.ok(match.group(1));
+				assert.ok(match.group(2));
+				assert.strictEqual(match.group(1).success, true);
+				assert.strictEqual(match.group(2).success, false);
+			});
+			test("named groups", () =>
+			{
+				const match = new ncre.Regex("(a+)b+(?<A>b+)?").match("aaabbb");
+				assert.ok(match);
+				assert.strictEqual(match.success, true);
+				assert.ok(match.group("1"));
+				assert.ok(match.group("A"));
+				assert.strictEqual(match.group("1").success, true);
+				assert.strictEqual(match.group("A").success, false);
+			});
+			test("nonexistent numbered groups", () =>
+			{
+				const match = new ncre.Regex("a(b)").match("ab");
+				assert.ok(match);
+				assert.strictEqual(match.success, true);
+				assert.strictEqual(match.group(2), undefined);
+			});
+			test("nonexistent named groups", () =>
+			{
+				const match = new ncre.Regex("a(?<A>b)").match("ab");
+				assert.ok(match);
+				assert.strictEqual(match.success, true);
+				assert.strictEqual(match.group("B"), undefined);
+			});
 		});
 	});
 });
 
 function testParse(feature, regex, only)
 {
-	(only ? it.only : it)(feature, () =>
+	(only ? test.only : test)(feature, () =>
 	{
 		assert.doesNotThrow(() => new ncre.Regex(regex), "NCRE parsing threw an error.");
 		assert.doesNotThrow(() => dotnet.parse({ regex }), ".NET parsing threw an error.");
@@ -142,7 +204,7 @@ function testParse(feature, regex, only)
 
 function testParseError(feature, regex, only)
 {
-	(only ? it.only : it)(feature, () =>
+	(only ? test.only : test)(feature, () =>
 	{
 		assert.throws(() => new ncre.Regex(regex), "NCRE parsing did not throw an error.");
 		assert.throws(() => dotnet.parse({ regex }), ".NET parsing did not throw an error.");
@@ -152,7 +214,7 @@ function testParseError(feature, regex, only)
 function testMatch(feature, regex, input, options, only)
 {
 	options = options || {};
-	(only ? it.only : it)(feature, () =>
+	(only ? test.only : test)(feature, () =>
 	{
 		const actual = new ncre.Regex(regex, options).match(input);
 		const expected = dotnet.match({ regex, input, options }, true);
